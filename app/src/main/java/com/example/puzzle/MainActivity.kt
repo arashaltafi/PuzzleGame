48f3e86a9d3656d65544b1bcae405d5f74b254ae
Arash Altafi <arashaltafi1377@gmail.com>
2023-05-11 19:32:11 +0430

    Initial commit

---
+++ app/src/main/java/com/example/puzzle/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.puzzle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.puzzle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
